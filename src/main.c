f4886f7051b50a3311e76c39077f3da573a95604
Casper Børretzen <casper.eide@gmail.com>
2024-01-02 00:57:36 +0100

    second
    - Added draw rectangle function
    - Added background grid
    - Added fullscreen

--- src/main.c
+++ src/main.c
@@ -21,6 +21,12 @@ bool initialize_window(void) {
 		return false;
 	}
 	
+	// Use SDL to query what the fullscreen max width and height
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
 	// Create a SDL Window
 	window = SDL_CreateWindow(
 		NULL, 
@@ -41,6 +47,8 @@ bool initialize_window(void) {
 		fprintf(stderr, "Error creating SDL renderer.\n");
 		return false;
 	}
+
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
 	return true;
 }
@@ -94,12 +102,44 @@ void clear_color_buffer(uint32_t color) {
 		}
 }
 
+void draw_grid(void) {
+	for (int y = 0; y < window_height; y += 10) {
+		for (int x = 0; x < window_width; x += 10) {
+			color_buffer[(window_width * y) + x] = 0xFF444444;
+		}
+	}
+
+}
+
+void draw_rect(int start_x, int start_y, int width, int height, uint32_t color) {
+	for (int y = start_y; y < (start_y + height); y++) {
+		for (int x = start_x; x < (start_x + width); x++) {
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
+/*
+void draw_rect_alt(int x, int y, int width, int height, uint32_t color) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			int current_x = x + i;
+			int current_y = y + j;
+			color_buffer[(window_width * current_y) + current_x] = color;
+		}
+	}
+}
+*/
+
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
 	
+	draw_grid();
+	draw_rect(200, 200, 300, 150, 0xFFFF0000);
+
 	render_color_buffer();
-	clear_color_buffer(0xFFFFFF00);
+	clear_color_buffer(0xFF111111);
 
 	SDL_RenderPresent(renderer);
 }
